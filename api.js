4917430700c0d83eba1e9ead9b0dc2d629ac0104
derudo <50494134+derudo@users.noreply.github.com>
2023-12-30 23:36:22 +0100

    weg mit puppeteer und drucker

--- api.js
+++ api.js
@@ -28,38 +28,7 @@ app.post('/updateContent', (req, res) => {
     currentContent = req.body;
     res.send('Content updated');
 
-    //puppeteer
-    /* (async() => {
-         const browser = await puppeteer.launch();
-         const page = await browser.newPage();
 
-         // Navigate the page to a URL
-         await page.goto('http://localhost:8000/');
-
-         await page.pdf({
-             format: 'A4'
-         }).then((buffer) => {
-             http.request({
-                     'hostname': targetIP,
-                     'path': "/printers",
-                     'auth': apiKey + ":"
-                 },
-                 function(response) {
-                     console.log('STATUS: ' + response.statusCode);
-                     response.setEncoding('utf8');
-                     response.on('data', function(chunk) {
-                         console.log('BODY: ' + chunk);
-                         //Id vom richtigen Drucker raussuchen
-                         //Printjob zusammenbauen
-                         //Post an richtigen Drucker
-                     });
-                 });
-
-         });
-
-         await browser.close();
-
-     })();*/
 });
 
 // Aktueller inhalt abrufen
