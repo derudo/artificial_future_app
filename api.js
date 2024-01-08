41083193cdd080c292e64d86a51fca69c32854b5
Jan Osterkamp <jan.osterkamp@dextradata.com>
2024-01-08 20:22:42 +0100

    Fixed Queue

--- api.js
+++ api.js
@@ -4,10 +4,10 @@ const cors = require('cors');
 
 const app = express();
 const port = 8000;
-const puppeteer = require('puppeteer');
+//const puppeteer = require('puppeteer');
 const http = require('http');
 
-const targetIP = "" // TODO: IP vom Laptop
+const targetIP = ""; // TODO: IP vom Laptop
 const apiKey = ""; //TODO: API Key einfügen
 
 let html;
@@ -17,23 +17,41 @@ let currentContent = {
     imageUrl: "standard-img-url"
 };
 
+let goodQueue = [];
+let evilQueue = [];
+
 app.use(cors());
 
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.post('/updateContent', (req, res) => {
-    currentContent = req.body;
+    if(req.body.gesinnung == 0){
+        goodQueue.push(req.body);
+    } else{
+        evilQueue.push(req.body);
+    }
+    
     res.send('Content updated');
-
-
 });
 
 // Aktueller inhalt abrufen
-app.get('/getContent', (req, res) => {
-    res.json(currentContent);
+app.get('/getGood', (req, res) => {
+    if(goodQueue.length > 0){
+        res.json(goodQueue.shift());
+    } else {
+        res.json("");
+    }
+});
+
+app.get('/getEvil', (req, res) => {
+    if(evilQueue.length > 0){
+        res.json(evilQueue.shift());
+    } else {
+        res.json("");
+    }
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
